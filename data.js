c30c0453f00cfa9e562c17df4cc353df76371e3c
rutvikapuriya9161 <rutvikapuriya9161@gmail.com>
2022-05-30 15:30:01 +0530

    30_5_22

---
+++ data.js
@@ -0,0 +1,86 @@
+const person = {
+    name: "Amit",
+    age: 25
+}
+
+
+const person = [
+    {
+        name: "Amit",
+        age: 25,
+    },
+    {
+        name: "Piyush",
+        age: 40,
+    },
+
+
+];
+
+
+const person = {
+    name: "Amit",
+    age: 25,
+    course: [
+        "C",
+        "HTML"
+    ]
+}
+
+
+const person = [
+    {
+        name: "Amit",
+        age: 25,
+        course: [
+            "C",
+            "HTML"
+        ]
+    },
+    {
+        name: "Ajay",
+        age: 40,
+        course: [
+            "Java",
+            "JavaScript"
+        ]
+    }
+]
+
+
+const myObj = {
+    name: "John",
+    age: 30,
+    cars: {
+        car1: "Ford",
+        car2: "BMW",
+        car3: "Fiat"
+    }
+}
+
+const data = {
+    personal_info: {
+        name: 'amit',
+        age: 25,
+        city: 'surat'
+    },
+    courses: ["C", "JavaScript", "React"],
+    branches: {
+        rwl: {
+            admission: 50,
+            vacant_seat: 10
+        },
+        rw2: {
+            admission: 30,
+            vacant_seat: 20
+        },
+        rw3: {
+            admission: 25,
+            vacant_seat: 25
+        },
+        rw4: {
+            admission: 40,
+            vacant_seat: 10
+        }
+    }
+}
--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,8 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 import Demo from './Component/Demo';
-
 function App() {
+  const data = {
+    personal_info: {
+      name: 'amit',
+      age: 25,
+      city: 'surat'
+    },
+    courses: ["C", "JavaScript", "React"],
+    branches: {
+      rwl: {
+        admission: 50,
+        vacant_seat: 10
+      },
+      rw2: {
+        admission: 30,
+        vacant_seat: 20
+      },
+      rw3: {
+        admission: 25,
+        vacant_seat: 25
+      },
+      rw4: {
+        admission: 40,
+        vacant_seat: 10
+      }
+    }
+  }
+
+  for (let k in data) {
+    if (k === 'personal_info') {
+      console.log(data[k].name, data[k].age, data[k].city);
+    } else if (k === 'courses') {
+      data[k].map((d) => console.log((d)));
+    } else {
+      for (let j in data[k]) {
+        console.log((data[k][j].admission, data[k][j].vacant_seat));
+      }
+    }
+  }
+
   return (
     <Demo />
   );
