553973c338f8637d8c1d4b0d6c397ba827433f05
rutvikapuriya9161 <rutvikapuriya9161@gmail.com>
2022-05-31 15:23:09 +0530

    31_05_22

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,61 +5,68 @@ function App() {
   let data =
   [
     {
-      name: "amit",
-      age: 35,
-      salary: 40000,
-      bonus: 1000,
+      id: 101,
+      name: 'Abacavir',
+      quantity: 25,
+      price: 150,
+      expiry: 2022,
       status: true
     },
     {
-      name: "ajay",
-      age: 25,
-      salary: 38000,
-      bonus: 2000,
+      id: 102,
+      name: 'Eltrombopag',
+      quantity: 90,
+      price: 550,
+      expiry: 2021,
+      status: true
+    },
+    {
+      id: 103,
+      name: 'Meloxicam',
+      quantity: 85,
+      price: 450,
+      expiry: 2025,
       status: false
     },
     {
-      name: "mayur",
-      age: 23,
-      salary: 50000,
-      bonus: 500,
+      id: 104,
+      name: 'Allopurinol',
+      quantity: 50,
+      price: 600,
+      expiry: 2023,
       status: true
     },
     {
-      name: "jay",
-      age: 29,
-      salary: 35000,
-      bonus: 600,
-      status: true
-    },
-    {
-      name: "raj",
-      age: 33,
-      salary: 22000,
-      bonus: 2000,
-      status: true
-    },
-  ]
+      id: 105,
+      name: 'Phenytoin',
+      quantity: 63,
+      price: 250,
+      expiry: 2021,
+      status: false
+    }
+  ];
 
 
   return (
     <div>
       <table border="1">
         <tr>
+          <th>id</th>
           <th>name</th>
-          <th>age</th>
-          <th>salary</th>
-          <th>bonus</th>
+          <th>quantity</th>
+          <th>price</th>
+          <th>expiry</th>
           <th>status</th>
         </tr>
         {
           data.map((d,i) => {
             return(
             <tr>
+              <td>{d.id}</td>
               <td>{d.name}</td>
-              <td>{d.age}</td>
-              <td>{d.salary}</td>
-              <td>{d.bonus}</td>
+              <td>{d.quantity}</td>
+              <td>{d.price}</td>
+              <td>{d.expiry}</td>
               <td>{d.status.toString()}</td>
             </tr>
             )
